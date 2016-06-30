f94f376fd2ec68885e8b6a6649e0eac34175ca5a
Pozsar Zsolt <pozsarzs@gmail.com>
2016-06-30 20:09:00 +0000

    #160630

---
+++ source/frmdrawer.pp
@@ -0,0 +1,45 @@
+{ +--------------------------------------------------------------------------+ }
+{ | Tubes2 2.2 trial * Electrontube catalogue                                | }
+{ | Copyright (C) 2008-2016 Pozsar Zsolt <pozsarzs@gmail.com>                | }
+{ | frmdrawer.pp                                                             | }
+{ | Characteristic drawer tool                                               | }
+{ +--------------------------------------------------------------------------+ }
+
+{
+   This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or (at your
+ option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+}
+
+unit frmdrawer;
+{$MODE OBJFPC}
+interface
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs;
+type
+  TForm10 = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+var
+  Form10: TForm10;
+  viewer: boolean;
+implementation
+ {$R *.lfm}
+
+initialization
+
+
+end.
+
